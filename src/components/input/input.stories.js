18c49aa41c6464689689d16c519c4173cb2a7033
christophvogel <christoph@boerd.net>
2021-01-26 14:39:26 +0100

    Add components

---
+++ src/components/input/input.stories.js
@@ -0,0 +1,9 @@
+import "./input.css";
+import buttonInput from "./input.html";
+
+export default {
+  title: "Components/Input",
+  parameters: { layout: "centered" },
+};
+
+export const InputField = () => buttonInput;
